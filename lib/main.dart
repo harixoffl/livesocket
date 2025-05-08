cd0fbe3f8895d7c2031cc3139885c61e297cd57d
HARI <hariprasath.s@sporadasecure.com>
2025-05-08 12:17:40 +0530

    fd

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class _WebRTCVideoStreamState extends State<WebRTCVideoStream> {
     // WebRTC Configuration
     Map<String, dynamic> config = {
       'iceServers': [
-        {'urls': 'stun:stun.l.google.com:19301'},
+        {'urls': 'stun:stun.l.google.com:19305'},
       ],
     };
 
